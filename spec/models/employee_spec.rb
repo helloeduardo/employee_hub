9bcb61ed02ea38198f5d7f3b9937321a2de59a24
Eduardo Parra <helloeduardo@icloud.com>
2021-03-25 23:49:25 -0600

    Add employee model spec

---
+++ spec/models/employee_spec.rb
@@ -0,0 +1,12 @@
+describe Employee do
+  describe "Validations" do
+    it {should validate_presence_of :email}
+    it {should validate_uniqueness_of :email}
+    it {should validate_presence_of :name}
+  end
+
+  describe "Relationship" do
+    it { should have_many(:subordinates) }
+    it { should belong_to(:manager).optional }
+  end
+end
