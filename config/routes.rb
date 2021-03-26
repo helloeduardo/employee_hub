ba5a3c9a29fa016a6df8f7a19f5a2e83373d46c6
Eduardo Parra <helloeduardo@icloud.com>
2021-03-25 23:50:40 -0600

    Add auth flow routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "welcome#index"
+
+  post '/login', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy'
+
+  get '/dashboard', to: 'dashboard#index'
+
 end
